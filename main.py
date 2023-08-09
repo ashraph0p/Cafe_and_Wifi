4cd8a1ba6a3ec262416f12bf14c1f2b775a9242f
M.A <ashrahp0p1@gmail.com>
2023-08-09 04:04:24 +0300

    commit init

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/cafe-list')
+def cafe_list():
+    with open("cafe_data.csv", newline='') as file_csv:
+        data = csv.reader(file_csv, delimiter=',')
+        list_of_rows = []
+        for row in data:
+            list_of_rows.append(row)
+    return render_template('list.html', data=list_of_rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
